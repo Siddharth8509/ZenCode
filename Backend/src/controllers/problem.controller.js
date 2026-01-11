57b583d26f46293b7902c9e037087ce91f46ccd2
Siddharth Roy <siddharthr8509@gmail.com>
2026-01-12 00:05:04 +0530

    Done with creating the api for problems

--- Backend/src/controllers/problem.controller.js
+++ Backend/src/controllers/problem.controller.js
@@ -1,6 +1,7 @@
 import {getLanguageId,submitBatch,submitToken} from "../utils/problem.utils.js";
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 import problem from "../model/problem.js";
+import user from "../model/user.js"
 
 const createProblem = async(req,res)=>{
     const {
@@ -53,4 +54,117 @@ const createProblem = async(req,res)=>{
     }
 }
 
-export default createProblem;
+const getProblemById = async (req, res) => {
+  try 
+  {
+    const { id } = req.params;
+    if (!id) 
+      return res.status(400).send("Problem id is required");
+    
+
+    if (!mongoose.Types.ObjectId.isValid(id)) 
+      return res.status(400).send("Invalid problem id");
+    
+
+    const problemDoc = await problem.findById(id).select("title");
+
+    if (!problemDoc)
+      return res.status(404).send("Problem does not exist");
+    
+
+    return res.status(200).json({
+      title: problemDoc.title
+    });
+  } 
+  catch (error) 
+  {
+    return res.status(500).send(error.message);
+  }
+};
+
+const problemFetchAll = async(req,res) => {
+    try 
+    {
+        const titles = await problem.find().select("title");
+        res.status(200).send(titles);
+    } 
+    catch (error) 
+    {
+        res.status(500).send("Error while fetching data")
+    }
+}
+
+const updateProblem = async(req,res) => {
+    try 
+    {
+        const data = req.body;
+        if(!data)
+            res.status(404).send("Please enter data to update");
+
+        const {id} = req.params;
+        if(!id)
+            res.status(404).send("Please send the id of the problem")
+
+        if(!mongoose.Types.ObjectId.isValid(id))
+            res.status(404).send("Please enter a valid id")
+
+        const updatedProblem = await problem.findByIdAndUpdate(id,data,{
+            new : true,
+            runValidators : true
+        })
+
+        res.status(200).send("Problem updated successfully : " + updatedProblem.title);
+    } 
+    catch (error) 
+    {
+        res.status(500).send("Error occured while updating"+error)
+    }
+}
+
+const solvedProblemByUser = async(req,res) => {
+    try 
+    {
+        const userId = req.userId;
+        if (!userId) 
+            return res.status(401).send("Unauthorized");
+
+        if (!mongoose.Types.ObjectId.isValid(userId)) 
+            return res.status(400).send("Invalid user id");
+    
+
+        const userData = await user.findById(userId).select("problemSolved");
+
+        if (!userData) 
+            return res.status(404).send("User not found");
+    
+
+        return res.status(200).json({solvedCount: userData.problemSolved.length,problems: userData.problemSolved});
+
+    } 
+    catch (error) 
+    {
+        return res.status(500).send("Failed to fetch data");
+    }
+}
+
+const deleteProblem = async(req,res) => {
+    try 
+    {
+        const {id} = req.params;
+        if(!id)
+            res.status(404).send("Please enter a valid id");
+        
+        if(!mongoose.Types.ObjectId.isValid(id))
+            res.status(404).send("Invalid id")
+
+        const deletedProblem = await problem.findByIdAndDelete(id);
+
+        return res.status(200).send("Problem deleted successfully");
+    } 
+    catch (error) 
+    {
+        return res.status(500).send("An error occured while deleting the problem");     
+    }
+}
+
+export  {createProblem,getProblemById,problemFetchAll,updateProblem,solvedProblemByUser,deleteProblem};
--- Backend/src/routes/problem.routes.js
+++ Backend/src/routes/problem.routes.js
@@ -1,18 +1,18 @@
 import express from "express";
 import adminMiddleware from "../middleware/admin.middleware.js";
 const problemRouter = express.Router();
-import createProblem from "../controllers/problem.controller.js" 
+import {createProblem,getProblemById,problemFetchAll,updateProblem,solvedProblemByUser,deleteProblem} from "../controllers/problem.controller.js" 
+import authMiddleware from "../middleware/auth.middleware.js";
 
 //In this we just have to validate the admin
 problemRouter.post("/create",adminMiddleware,createProblem);
+problemRouter.get("/:id",adminMiddleware,getProblemById);
+problemRouter.get("/",adminMiddleware,problemFetchAll);
+problemRouter.patch("/update/:id",adminMiddleware,updateProblem);
+problemRouter.delete("/delete/:id",adminMiddleware,deleteProblem);
 
 
-// problemRouter.get("/:id",adminMiddleware,getProblemById);
-// problemRouter.get("/",adminMiddleware,problemFetchAll);
-
-// //In this we just have to validate the user
-// problemRouter.patch("/update/:id",updateProblem);
-// problemRouter.delete("/delete/:id",deleteProblem);
-// problemRouter.get("/user",solvedAllProblemByUser);
+//In this we just have to validate the user
+problemRouter.get("/user",authMiddleware,solvedProblemByUser);
 
 export default problemRouter;
